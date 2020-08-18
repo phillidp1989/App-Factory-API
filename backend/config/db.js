1167fce71ec9e5962ae8dc0ead703b25d561a5d7
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-18 19:21:42 +0100

    Formatted document

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const { db } = require('./config');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db.uri || 'mongodb://localhost/app_factory', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        });
-        console.log('Connected to DB'.magenta);
-        return true;
-    } catch (err) {
-        console.error('ERROR - db.js - connectDB: ', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(db.uri || 'mongodb://localhost/app_factory', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log('Connected to DB'.magenta);
+    return true;
+  } catch (err) {
+    console.error('ERROR - db.js - connectDB: ', err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
