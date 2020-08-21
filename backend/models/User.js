b87615e7654be4f03e22e6221026581a3f1f32bb
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-21 22:24:31 +0100

    Creation of User schema

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// Creation of User schema
+const UserSchema = new mongoose.Schema(
+  {
+    provider: {
+      type: String,
+      required: true,
+    },
+    providerId: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    username: {
+      type: String,
+    },
+    displayName: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    isDeveloper: {
+      type: Boolean,
+      default: false
+    },
+    // Reference to document in the Post collection to allow populate method to be used 
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
