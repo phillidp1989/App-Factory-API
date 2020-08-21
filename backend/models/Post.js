510f28ff7812dcedde7fda17e01385cdb5e33d93
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-21 22:24:42 +0100

    Creation of Post schema

---
+++ backend/models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Creation of Post schema
+
+const PostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true,
+    max: 200
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  technologies: [{ type: String }],
+  score: {
+    type: Number
+  },
+  posterId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  activeDevelopers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  likers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
