3b6002ca5a3a26703647311f81a125f20b147c7f
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-25 22:51:57 +0100

    Imported routes and passport strategies to server

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require('express');
+const passport = require('passport');
+const logger = require('morgan');
+const cookieSession = require('cookie-session');
 
-const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const config = require('./config/config');
+const connectDB = require('./config/db');
+
+// OAuth passport strategies
+require('./config/githubPassport');
+require('./config/googlePassport');
+require('./config/facebookPassport');
+
+// Environment variables
+require('dotenv').config();
 
 // Initiate express
 const app = express();
 
+// Initialize Morgan logger
+app.use(logger('dev'));
+
+// Cookie Session setup to ensure auth persists for specified amount of time
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [config.cookie.key]
+}));
+
 // Middleware initialisation to enable data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Initialize passport and sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 // Connect to DB
 connectDB();
