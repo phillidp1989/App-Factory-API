5a9de584ffd980eba58bf0fcc296e714dc79fa9d
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-09-07 19:19:11 +0100

    Context fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
+  credentials: 'include'
 }));
 
 // Initialize Morgan logger
